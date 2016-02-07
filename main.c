dfe62dffa9efe3e5c8d683910d8e9ec94235a0b1
mmwebster <milo.webster@gmail.com>
2016-02-06 17:46:50 -0800

    final

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ int main (int argc, char **argv)
 {
   char final [1024];
   char buff[256];
+
+  // get url
   if (argc > 1) {
     sprintf(buff, "open http://www.%s.com -a Firefox", argv[1]);
   } else {
@@ -17,9 +19,17 @@ int main (int argc, char **argv)
   // open first time
   system(buff);
 
+  // get num of iterations
+  int num = 5;
+  /* if (argc > 2) { */
+  /*   num = *argv[2]; */
+  /* } else { */
+  /*   num = 5; */
+  /* } */
+
   // open again for interval
   int j;
-  for (j = 0; j < 5; j++) {
+  for (j = 0; j < num; j++) {
     long unsigned int i;
     for (i = 0; i < (TIME); i++) {
     }
