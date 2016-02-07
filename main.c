5c747bdda5e8034964d328a7362e9c28b6d54579
mmwebster <milo.webster@gmail.com>
2016-02-06 17:43:30 -0800

    its ready

--- main.c
+++ main.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SECOND 455000000 // VERY hardware dependent
+#define TIME 99500000000 // VERY hardware dependent
+
+// will open the provided url five times with TIME delay
 
 int main (int argc, char **argv)
 {
   char final [1024];
   char buff[256];
   if (argc > 1) {
-    sprintf(buff, "%s", argv[1]);
-    /* strcpy(buff, argv[2]); */
+    sprintf(buff, "open http://www.%s.com -a Firefox", argv[1]);
   } else {
-    return 0;
+    sprintf(buff, "open %s -a Firefox", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
   }
-  sprintf(final, "%s%s%s", "curl http://www.", argv[1], ".com > .surprise.html");
-  system(final);
 
-  while (1) {
+  // open first time
+  system(buff);
+
+  // open again for interval
+  int j;
+  for (j = 0; j < 5; j++) {
     long unsigned int i;
-    for (i = 0; i < (SECOND * (int)argv[2]); i++) {}
-    /* delay(); */
-    system("open .surprise.html -a Firefox");
+    for (i = 0; i < (TIME); i++) {
+    }
+    system(buff);
   }
 
 }
